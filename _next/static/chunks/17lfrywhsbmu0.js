aa6dc74876322b9abec9d1f43dfcc2c0db4c6af1
princenurch <codingnurch@gmail.com>
2026-06-17 16:51:42 +0500

    Deploy light Neurolab redesign

--- _next/static/chunks/17lfrywhsbmu0.js
+++
@@ -1,1 +0,0 @@
-(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,48222,t=>{t.v(c=>Promise.all(["static/chunks/0h4sc52jaty_-.js"].map(c=>t.l(c))).then(()=>c(56825)))}]);
